f24e28bc6d9e9b3aaacfbeba4a5a6648559f58a7
saulamsal <saulamsal@gmail.com>
2024-04-10 23:12:53 -0400

    added custom package for collapsible tab view + cleanup homepage UI + made news feed modular

--- src/MaterialTabBar/TabItem.tsx
+++ src/MaterialTabBar/TabItem.tsx
@@ -1,13 +1,13 @@
-import React, { useMemo } from 'react'
-import { StyleSheet, Pressable, Platform } from 'react-native'
+import React, {useMemo} from 'react'
+import {StyleSheet, Pressable, Platform} from 'react-native'
 import Animated, {
-  Extrapolation,
-  interpolate,
-  useAnimatedStyle,
+    Extrapolation,
+    interpolate,
+    useAnimatedStyle,
 } from 'react-native-reanimated'
 
-import { MaterialTabItemProps } from './types'
-import { TabName } from '../types'
+import {MaterialTabItemProps} from './types'
+import {TabName} from '../types'
 
 export const TABBAR_HEIGHT = 48
 const DEFAULT_COLOR = 'rgba(0, 0, 0, 1)'
@@ -16,90 +16,89 @@ const DEFAULT_COLOR = 'rgba(0, 0, 0, 1)'
  * Any additional props are passed to the pressable component.
  */
 export const MaterialTabItem = <T extends TabName = string>(
-  props: MaterialTabItemProps<T>
+    props: MaterialTabItemProps<T>
 ): React.ReactElement => {
-  const {
-    name,
-    index,
-    onPress,
-    onLayout,
-    scrollEnabled,
-    indexDecimal,
-    label,
-    style,
-    labelStyle,
-    activeColor = DEFAULT_COLOR,
-    inactiveColor = DEFAULT_COLOR,
-    inactiveOpacity = 0.7,
-    pressColor = '#DDDDDD',
-    pressOpacity = Platform.OS === 'ios' ? 0.2 : 1,
-    ...rest
-  } = props
-
-  const stylez = useAnimatedStyle(() => {
-    return {
-      opacity: interpolate(
-        indexDecimal.value,
-        [index - 1, index, index + 1],
-        [inactiveOpacity, 1, inactiveOpacity],
-        Extrapolation.CLAMP
-      ),
-      color:
-        Math.abs(index - indexDecimal.value) < 0.5
-          ? activeColor
-          : inactiveColor,
-    }
-  })
-
-  const renderedLabel = useMemo(() => {
-    if (typeof label === 'string') {
-      return (
-        // <Animated.Text style={[styles.label, stylez, labelStyle]}>
-          <Animated.Text style={[styles.label, stylez, labelStyle,
-            (Math.abs(index - indexDecimal.value) < 0.5 && props.activeLabelStyle)
-          ]}>
-          {label}
-        </Animated.Text>
-      )
-    }
-
-    return label(props)
-  }, [label, labelStyle, props, stylez])
-
-  return (
-    <Pressable
-      onLayout={onLayout}
-      style={({ pressed }) => [
-        { opacity: pressed ? pressOpacity : 1 },
-        (!scrollEnabled && !props.allowTabScroll) && styles.grow,
-        styles.item,
+    const {
+        name,
+        index,
+        onPress,
+        onLayout,
+        scrollEnabled,
+        indexDecimal,
+        label,
         style,
-        (Math.abs(index - indexDecimal.value) < 0.5 &&
-            props.activeTabStyle)
-      ]}
-      onPress={() => onPress(name)}
-      android_ripple={{
-        borderless: true,
-        color: pressColor,
-      }}
-      {...rest}
-    >
-      {renderedLabel}
-    </Pressable>
-  )
+        labelStyle,
+        activeColor = DEFAULT_COLOR,
+        inactiveColor = DEFAULT_COLOR,
+        inactiveOpacity = 0.7,
+        pressColor = '#DDDDDD',
+        pressOpacity = Platform.OS === 'ios' ? 0.2 : 1,
+        ...rest
+    } = props
+
+    const stylez = useAnimatedStyle(() => {
+        return {
+            opacity: interpolate(
+                indexDecimal.value,
+                [index - 1, index, index + 1],
+                [inactiveOpacity, 1, inactiveOpacity],
+                Extrapolation.CLAMP
+            ),
+            color:
+                Math.abs(index - indexDecimal.value) < 0.5
+                    ? activeColor
+                    : inactiveColor,
+        }
+    })
+
+    const renderedLabel = useMemo(() => {
+        if (typeof label === 'string') {
+            return (
+                // <Animated.Text style={[styles.label, stylez, labelStyle]}>
+                <Animated.Text style={[styles.label, stylez, labelStyle,
+                    (Math.abs(index - indexDecimal.value) < 0.5 && props.activeLabelStyle)
+                ]}>
+                    {label}
+                </Animated.Text>
+            )
+        }
+
+        return label(props)
+    }, [label, labelStyle, props, stylez])
+    return (
+        <Pressable
+            onLayout={onLayout}
+            style={({pressed}) => [
+                {opacity: pressed ? pressOpacity : 1},
+                (!scrollEnabled && !props.allowTabScroll) && styles.grow,
+                styles.item,
+                style,
+                (Math.abs(index - indexDecimal.value) < 0.5 &&
+                    props.activeTabStyle)
+            ]}
+            onPress={() => onPress(name)}
+            android_ripple={{
+                borderless: true,
+                color: pressColor,
+            }}
+            {...rest}
+        >
+            {renderedLabel}
+        </Pressable>
+    )
 }
 
 const styles = StyleSheet.create({
-  grow: {
-    flex: 1,
-  },
-  item: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingHorizontal: 10,
-    height: TABBAR_HEIGHT,
-  },
-  label: {
-    margin: 4,
-  },
+    grow: {
+        flex: 1,
+    },
+    item: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingHorizontal: 10,
+        height: TABBAR_HEIGHT,
+    },
+    label: {
+        margin: 4,
+    },
 })
